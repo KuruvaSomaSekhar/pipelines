dff38ae18162d8c086dc5317ea910888bc522bbb
Soma Sekhar Kuruva <somasekhar.kuruva@trianz.com>
2022-02-07 07:58:10 +0530

    build Jenkinsfile

---
+++ Jenkinsfile_build.groovy
@@ -0,0 +1,26 @@
+pipeline{
+    agent any
+    stages{
+        stage("helloworld"){
+            steps{
+                println "Welcome to Jenkins pipeline world"
+            }
+        }
+        stage("build"){
+            steps {
+                print "Here I'm building artifacts file"
+                sh "mvn clean package"
+            }
+        }
+        stage("upload"){
+            steps {
+                println "Here We will update artifacts to S3 buckets"
+            }
+        }
+        stage("deploy"){
+            steps {
+                println "Here we deploy artifacts to any environment"
+            }
+        }
+    }
+}
