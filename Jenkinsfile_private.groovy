a6ba6bd07e232765545bab007f1b2ff2d8803617
Soma Sekhar Kuruva <somasekhar.kuruva@trianz.com>
2022-02-08 08:01:13 +0530

    added checkout step

--- Jenkinsfile_private.groovy
+++ Jenkinsfile_private.groovy
@@ -5,6 +5,9 @@ pipeline {
         stage("clone"){
             steps {
                 println "Here we download source code from the private repo"
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git-auth2', url: 'https://github.com/KuruvaSomaSekhar/privatecode.git']]])
+                sh "ls -lart ./*"
+        
             }
         }
         stage("build"){
