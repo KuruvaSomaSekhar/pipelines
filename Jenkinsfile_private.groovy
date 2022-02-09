843c3c0b82c2562314563d70a4782bf4e3bc6a30
Soma Sekhar Kuruva <somasekhar.kuruva@trianz.com>
2022-02-09 07:34:01 +0530

    added variables

--- Jenkinsfile_private.groovy
+++ Jenkinsfile_private.groovy
@@ -14,8 +14,14 @@ pipeline {
         stage("clone"){
             steps {
                 println "Here we download source code from the private repo"
-                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git-auth2', url: 'https://github.com/KuruvaSomaSekhar/privatecode.git']]])
-                sh "ls -lart ./*"
+                checkout([$class: 'GitSCM', branches: [[name: ${codebranch}]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git-auth2', url: 'https://github.com/KuruvaSomaSekhar/privatecode.git']]])
+                sh """ls -lart ./*
+                    echo  ${env. JOB_NAME} 
+                    echo ${codebranch}
+                    echo ${BUILD_NUMBER}
+
+                    
+                    """
         
             }
         }
